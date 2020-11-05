6238d06ac84221b9e4240f7ad7d954c233acfd4c
Bart Massey <bart.massey@gmail.com>
2020-11-05 11:28:55 -0800

    used numpy to speed up hamming

--- knn.py
+++ knn.py
@@ -3,16 +3,17 @@
 # Bart Massey
 
 import csv, math, random, sys
+import numpy as np
 
 # Show individual instance results.
-TRACE = True
+TRACE = False
 
 # Class of instances.
 class Instance(object):
     def __init__(self, row):
         self.name = row[0]
         self.label = int(row[1])
-        self.features = [int(f) for f in row[2:]]
+        self.features = np.array([int(f) for f in row[2:]])
 
 # Read instances.
 with open(sys.argv[1], "r") as f:
@@ -33,7 +34,7 @@ nfeatures = len(instances[0].features)
 
 # Hamming distance between feature vectors.
 def hamming(f1, f2):
-    return sum([abs(f1[i] - f2[i]) for i in range(nfeatures)])
+    return np.sum(np.abs(f1 - f2))
 
 # Try training on the training instances and then
 # classifying the test instances.  Return the classification
