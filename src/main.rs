7722dc4a4e324825f8e21427752186dbd0900098
Bart Massey <bart.massey@gmail.com>
2020-11-06 21:23:46 -0800

    added csv reading with serde deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod instance;
+use instance::*;
+
 use argh::FromArgs;
 
 #[derive(FromArgs)]
@@ -9,6 +12,9 @@ struct Args {
     /// learning algorithm
     #[argh(subcommand)]
     algorithm: ArgsAlg,
+    /// csv feature file (default stdin)
+    #[argh(positional)]
+    features: Option<String>,
 }
 
 #[derive(FromArgs)]
@@ -34,16 +40,33 @@ struct KNNArgs {
 
 fn main() {
     let args: Args = argh::from_env();
+    let features: Box<dyn std::io::Read> = match args.features {
+        None => Box::new(std::io::stdin()),
+        Some(name) => Box::new(std::fs::File::open(name).unwrap_or_else(
+            |e| {
+                eprintln!("could not open features: {}", e);
+                std::process::exit(1);
+            }
+        )),
+    };
+    let mut rdr = csv::ReaderBuilder::new().has_headers(false).from_reader(features);
+    let records: Vec<Instance> = rdr.deserialize().map(|r| r.unwrap_or_else(
+        |e| {
+            eprintln!("could not read record: {}", e);
+            std::process::exit(1);
+        },
+    )).collect();
+    println!("{:#?}", records);
+    match args.crossval {
+        None => println!("split50"),
+        Some(0) => println!("loocv"),
+        Some(n) => println!("crossval {}", n),
+    }
     match args.algorithm {
         ArgsAlg::NBayes(NBayesArgs{}) => println!("nbayes"),
         ArgsAlg::KNN(KNNArgs{k}) => match k {
             None => println!("nbayes 5"),
             Some(k) => println!("nbayes {}", k),
         },
-    }
-    match args.crossval {
-        None => println!("split50"),
-        Some(0) => println!("loocv"),
-        Some(n) => println!("crossval {}", n),
     }
 }
